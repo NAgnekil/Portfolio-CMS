4ab0498c5a3aede55d6aae06627f55db51446513
NAgnekil <nathalie.agnekil@gmail.com>
2026-01-17 13:52:34 +0100

    Added social links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import LinkedInIcon from '../images/linkedin-icon.png';
+import GitHubIcon from '../images/github-icon.png';
+import EmailIcon from '../images/email-icon.png';
 import '../styles/footer.scss';
 
 const Footer = () => {
@@ -29,6 +32,30 @@ const Footer = () => {
               src={data.contentfulAboutMe.logo.url}
               alt={data.contentfulAboutMe.name}
             />
+          </Link>
+        </div>
+
+        <div className="social-links">
+          <Link
+            to="https://www.linkedin.com/in/nagnekil"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={LinkedInIcon} alt="LinkedIn" />
+          </Link>
+          <Link
+            to="https://github.com/NAgnekil"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={GitHubIcon} alt="GitHub" />
+          </Link>
+          <Link
+            to="mailto:nathalie@agnekil.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={EmailIcon} alt="Email" />
           </Link>
         </div>
 
